a61fc9dd00c8804ed9900f3a7df85ee0d71e0625
kyzer <redazzkillah@gmail.com>
2023-09-23 11:17:25 +0200

    fix bug on scroll

--- js/main.js
+++ js/main.js
@@ -11,13 +11,51 @@ let certSection;
 let expSection;
 let visionSection;
 
+let headerOffset = 540;
+let heroOffset = 80;
+let mainTitleOffset = 300;
+let introOffset = 490;
+let cvOffset = 1490;
+let certOffet = 2190;
+let expOffset = 3380;
+let visionOffset = 4000;
+
 // event handlers
 
 function onScroll() {
-  let offset = 540;
-  if (scrollTopBtn && window.scrollY >= offset)
-    scrollTopBtn.style.right = "10px";
+  let scroll = window.scrollY;
+
+  if (scrollTopBtn && scroll >= headerOffset) scrollTopBtn.style.right = "10px";
   else scrollTopBtn.style.right = "-100px";
+
+  if (scroll <= heroOffset) {
+    appear(heroText);
+    growUp(heroBtn);
+  } else {
+    appearReverse(heroText, "0");
+    growUpReverse(heroBtn, "0");
+  }
+
+  // if (scroll > heroOffset && scroll <= mainTitleOffset) slideLeft(mainTitle);
+  // else slideLeftReverse(mainTitle, "-500px");
+
+  // if (scroll > mainTitleOffset && scroll <= introOffset)
+  //   slideRight(introSection);
+  // else slideRightReverse(introSection, "-100vw");
+
+  if (scroll > introOffset && scroll <= cvOffset) slideLeft(cvSection);
+  else slideLeftReverse(cvSection, "-100vw");
+
+  if (scroll > cvOffset && scroll <= certOffet) growUp(certSection);
+  else growUpReverse(certSection, "0");
+
+  if (scroll > certOffet && scroll <= expOffset) slideRight(expSection);
+  else slideRightReverse(expSection, "-100vw");
+
+  if (scroll > expOffset && scroll <= visionOffset) growUp(visionSection);
+  else growUpReverse(visionSection, "0");
+
+  console.log(scroll);
 }
 
 // functions
@@ -28,6 +66,7 @@ function contactMe() {
 function goTop() {
   window.scroll({ behavior: "smooth", top: 0 });
 }
+
 function slideTop(element) {
   element.style.top = "0";
 }
@@ -42,6 +81,22 @@ function growUp(element) {
 }
 function appear(element) {
   element.style.opacity = "1";
+}
+
+function slideTopReverse(element, reverse) {
+  element.style.top = reverse;
+}
+function slideLeftReverse(element, reverse) {
+  element.style.left = reverse;
+}
+function slideRightReverse(element, reverse) {
+  element.style.right = reverse;
+}
+function growUpReverse(element, reverse) {
+  element.style.transform = `scale(${reverse})`;
+}
+function appearReverse(element, reverse) {
+  element.style.opacity = reverse;
 }
 
 // entry point
@@ -59,15 +114,21 @@ window.onload = () => {
   visionSection = document.querySelector("#vision");
   visionTitle = document.querySelector(".vision_title");
   visionContent = document.querySelector(".vision_content");
+  let timeout = 500;
 
-  slideTop(header);
-  appear(heroText);
-  growUp(heroBtn);
-  slideLeft(mainTitle);
-  slideRight(introSection);
-  slideLeft(cvSection);
-  growUp(certSection);
-  slideLeft(expSection);
-  growUp(visionSection);
+  setTimeout(() => {
+    slideTop(header);
+    setTimeout(() => {
+      appear(heroText);
+      growUp(heroBtn);
+      setTimeout(() => {
+        slideLeft(mainTitle);
+        setTimeout(() => {
+          slideRight(introSection);
+        }, timeout);
+      }, timeout);
+    }, timeout);
+  }, timeout);
+
   window.onscroll = onScroll;
 };
