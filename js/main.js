693bb33b169b049e0bcbcfbc3b2f9c10a5a81797
kyzer <redazzkillah@gmail.com>
2023-09-23 07:10:57 +0200

    add base animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 // varialbes
 
+let header;
+let heroText;
+let heroBtn;
+let mainTitle;
+let introSection;
+let cvSection;
+let certSection;
 let scrollTopBtn;
 
 // event handlers
@@ -19,9 +26,40 @@ function contactMe() {
 function goTop() {
   window.scroll({ behavior: "smooth", top: 0 });
 }
+function slideTop(element) {
+  element.style.top = "0";
+}
+function slideLeft(element) {
+  element.style.left = "0";
+}
+function slideRight(element) {
+  element.style.right = "0";
+}
+function growUp(element) {
+  element.style.transform = "scale(1)";
+}
+function appear(element) {
+  element.style.opacity = "1";
+}
 
 // entry point
 window.onload = () => {
+  // init vars
+  header = document.querySelector("header");
+  heroText = document.querySelector(".hero-text");
+  heroBtn = document.querySelector(".hero-btn");
+  mainTitle = document.querySelector(".main-title");
   scrollTopBtn = document.querySelector(".scroll-top-btn");
+  introSection = document.querySelector("#intro");
+  cvSection = document.querySelector("#cv");
+  certSection = document.querySelector("#cert");
+
+  slideTop(header);
+  appear(heroText);
+  growUp(heroBtn);
+  slideLeft(mainTitle);
+  slideRight(introSection);
+  slideLeft(cvSection);
+
   window.onscroll = onScroll;
 };
